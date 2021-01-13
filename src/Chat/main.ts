b983795ca16ea555546a3617c7b952a47c674c91
Illia Khovanskyi <saskiuhia@gmail.com>
2021-01-13 19:48:17 +0200

    Fix chat

--- src/Chat/main.ts
+++ src/Chat/main.ts
@@ -1,5 +1,4 @@
 import { LocalEvents } from '../Constants/localEvents';
-import { RemoteEvents } from '../Constants/remoteEvents';
 
 abstract class Chat {
   public static Start(): void {
@@ -9,16 +8,11 @@ abstract class Chat {
     chat.markAsChat();
 
     mp.events.add(LocalEvents.ChatCursorToggle, (h: boolean) => Chat.OnChatCursorToggle(h));
-    mp.events.add(LocalEvents.ChatSendMessage, (m: string, c: string) => Chat.OnSendMessage(m, c));
   }
 
   private static OnChatCursorToggle(hidden: boolean): void {
     mp.gui.cursor.show(!hidden, !hidden)
   }
-
-  private static OnSendMessage(message: string, channel: string): void {
-    mp.events.callRemote(RemoteEvents.ChatSendMessage, message, channel)
-  }
 };
 
-Chat.Start();
+Chat.Start()
