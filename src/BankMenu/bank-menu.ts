24eee81ad0e6b5c96b1646ed2d48509190721634
Vladimir Abdullaev <a.voxagamer@gmail.com>
2021-01-19 17:22:18 +0300

    Add bank menu

---
+++ src/BankMenu/bank-menu.ts
@@ -0,0 +1,39 @@
+abstract class BankMenuUi {
+  public static Start(): void {
+    const buttonsTabLink = document.querySelectorAll('button.tablink') as NodeListOf<HTMLElement>;
+    console.log(buttonsTabLink);
+    buttonsTabLink.forEach((b) => b.addEventListener('click', () => BankMenuUi.ChangeTab(b)));
+  }
+
+  private static ChangeTab(selectedTab: HTMLElement): void {
+    console.log(selectedTab);
+    const selectedTabId = selectedTab.getAttribute('data-tab');
+    const selectedTabLinkId = selectedTab.id;
+
+    const tabs = document.getElementsByClassName('tab-content') as HTMLCollectionOf<HTMLElement>;
+    for (let i = 0; i < tabs.length; i++) {
+      const isSelectedTab = tabs[i].id === selectedTabId;
+      if (isSelectedTab) {
+        tabs[i].querySelector('div.tab-content')?.classList.add('active');
+        tabs[i].querySelector('button.tablink')?.classList.add('active');
+        tabs[i].classList.add('active');
+      } else {
+        tabs[i].classList.remove('active');
+        tabs[i].querySelector('div.tab-content.active')?.classList.remove('active');
+        tabs[i].querySelector('button.tablink.active')?.classList.remove('active');
+      }
+    }
+
+    const tabLinks = document.getElementsByClassName('tablink') as HTMLCollectionOf<HTMLElement>;
+    for (let i = 0; i < tabLinks.length; i++) {
+      const isSelectedTabLink = tabLinks[i].id === selectedTabLinkId;
+      if (isSelectedTabLink) {
+        tabLinks[i].classList.add('active');
+      } else {
+        tabLinks[i].classList.remove('active');
+      }
+    }
+  }
+};
+
+BankMenuUi.Start();
