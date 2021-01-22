e5446c72c29724d3771e0ea09b6e86017c49a913
Vladimir Abdullaev <a.voxagamer@gmail.com>
2021-01-22 03:22:48 +0300

    TypeScript

--- src/Authentication/authentication.ts
+++ src/Authentication/authentication.ts
@@ -2,6 +2,10 @@ abstract class AuthenticationUi {
   public static Start(): void {
     const buttonsTabLink = document.querySelectorAll('.tablink') as NodeListOf<HTMLElement>;
     buttonsTabLink.forEach((b) => b.addEventListener('click', () => AuthenticationUi.ChangeTab(b)));
+
+    AuthenticationUi.StartLoginTab();
+    AuthenticationUi.StartRegistrationTab();
+    AuthenticationUi.StartRecoveryTab();
   }
 
   private static ChangeTab(selectedTab: HTMLElement): void {
@@ -30,6 +34,53 @@ abstract class AuthenticationUi {
       }
     }
   }
+
+  private static StartLoginTab(): void {
+    const form = document.querySelector('#loginTab > form') as HTMLFormElement;
+    form.addEventListener('submit', () => AuthenticationUi.OnLoginFormSubmit(form));
+  }
+
+  private static OnLoginFormSubmit(form: HTMLFormElement): void {
+    const formData = new FormData(form);
+    const username = formData.get('username') as string;
+    const password = formData.get('password') as string;
+
+    // TODO: Call Rage MP ebat
+  }
+
+  private static StartRegistrationTab(): void {
+    const form = document.querySelector('#registrationTab > form') as HTMLFormElement;
+    form.addEventListener('submit', () => AuthenticationUi.OnRegistrationFormSubmit(form));
+  }
+
+  private static OnRegistrationFormSubmit(form: HTMLFormElement): void {
+    const formData = new FormData(form);
+    const username = formData.get('username') as string;
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+    const passwordConfirm = formData.get('passwordConfirm') as string;
+
+    if (password !== passwordConfirm) {
+      const errorMessage = document.querySelector('#registrationTab .denied') as HTMLElement;
+      errorMessage.hidden = false;
+      return;
+    }
+
+    // TODO: Call Rage MP ebat
+  }
+
+  private static StartRecoveryTab(): void {
+    const form = document.querySelector('#recoveryTab > form') as HTMLFormElement;
+    form.addEventListener('submit', () => AuthenticationUi.OnRecoveryFormSubmit(form));
+  }
+
+  private static OnRecoveryFormSubmit(form: HTMLFormElement) {
+    const formData = new FormData(form);
+    const recovery = formData.get('recovery') as string;
+
+    // TODO: Call Rage MP ebat
+  }
 };
 
-AuthenticationUi.Start(); 
+AuthenticationUi.Start();
+
