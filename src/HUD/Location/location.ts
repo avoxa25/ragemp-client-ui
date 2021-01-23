7efec0944919c7ba1fcd51808ec869b8a5d57d5a
Illia Khovanskyi <saskiuhia@gmail.com>
2021-01-23 02:39:29 +0200

    Fix HUD location

--- src/HUD/Location/location.ts
+++ src/HUD/Location/location.ts
@@ -7,12 +7,15 @@ class HudLocationUi {
     document.body.hidden = false;
   }
 
-  public Update(streetName: string, crossingRoad: string): void {
+  public Update(streetName: string, crossingRoad: string, zone: string): void {
     const streetNameElement = document.querySelector('#streetName') as HTMLElement;
     streetNameElement.innerText = streetName;
 
     const crossingRoadElement = document.querySelector('#crossingRoad') as HTMLElement;
     crossingRoadElement.innerText = crossingRoad;
+
+    const zoneElement = document.querySelector('#zone') as HTMLElement;
+    zoneElement.innerText = zone;
   }
 };
 
--- src/HUD/Location/main.ts
+++ src/HUD/Location/main.ts
@@ -1,21 +1,24 @@
 import { RemoteResponse } from '../../Constants/remote-response';
+import { ZoneData } from './zone-data';
 
 class HudLocation {
   private readonly browser: BrowserMp;
 
   constructor() {
     this.browser = mp.browsers.new('package://HUD/Location/location.html');
-    setInterval(() => this.Update(), 3000);
+    setInterval(() => this.Update(), 1000);
   }
 
   private Update(): void {
-    const playerStreet = mp.game.pathfind.getStreetNameAtCoord(mp.players.local.position.x, mp.players.local.position.y, mp.players.local.position.z, 0, 0);
+    const streetHashCodes = mp.game.pathfind.getStreetNameAtCoord(mp.players.local.position.x, mp.players.local.position.y, mp.players.local.position.z, 0, 0);
+    const streetName = mp.game.ui.getStreetNameFromHashKey(streetHashCodes.streetName);
+    const crossingRoad = mp.game.ui.getStreetNameFromHashKey(streetHashCodes.crossingRoad);
 
-    const streetName = mp.game.ui.getStreetNameFromHashKey(playerStreet.streetName);
-    const crossingRoad = mp.game.ui.getStreetNameFromHashKey(playerStreet.crossingRoad);
+    const zoneId = mp.game.zone.getNameOfZone(mp.players.local.position.x, mp.players.local.position.y, mp.players.local.position.z);
+    const zone = ZoneData.GetZoneName(zoneId);
 
-    this.browser.execute(`window.hudLocationUi.Update("${streetName}", "${crossingRoad}");`);
+    this.browser.execute(`window.hudLocationUi.Update("${streetName}", "${crossingRoad}", "${zone}");`);
   }
 };
 
-mp.events.add(RemoteResponse.LoginSuccess, () => new HudLocation());  
+mp.events.add(RemoteResponse.CharacterSelected, () => new HudLocation());  
