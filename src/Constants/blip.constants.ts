d523c3a46a56ab6c507e758f5a5f74cb41eeadf9
Illia Khovanskyi <saskiuhia@gmail.com>
2021-01-23 04:36:39 +0200

    Add GasStationBlipsSync

--- src/Constants/blip.constants.ts
+++ src/Constants/blip.constants.ts
@@ -11,9 +11,27 @@ export abstract class BlipConstants {
 
   public static readonly HouseOccupiedName = 'Занятый дом';
   public static readonly HouseOnSaleName = 'Сводобный дом';
-  public static readonly HouseOwnedName = 'Дом';
+  public static readonly HouseOwnedName = 'Ваш дом';
 
   public static readonly HouseRotation = 0;
   public static readonly HouseScale = 1;
   public static readonly HouseShortRange = true;
+
+  public static readonly GasStationSprite = 361;
+  public static readonly GasStationAlpha = 361;
+  public static readonly GasStationDimension = 0;
+
+  public static readonly GasStationOccupiedColor = 4;
+  public static readonly GasStationOnSaleColor = 2;
+  public static readonly GasStationOwnedColor = 3;
+
+  public static readonly GasStationDrawDistance = 30;
+
+  public static readonly GasStationOccupiedName = 'Заправка';
+  public static readonly GasStationOnSaleName = 'Доступная заправка';
+  public static readonly GasStationOwnedName = 'Ваша заправка';
+
+  public static readonly GasStationRotation = 0;
+  public static readonly GasStationScale = 1;
+  public static readonly GasStationShortRange = true;
 };
