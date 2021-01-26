5fc17a3edf931b00386c991ca76de9635db27526
loli32 <soltmik@yandex.ru>
2021-01-27 01:17:50 +0300

    Updated main.ts

--- src/Render/main.ts
+++ src/Render/main.ts
@@ -1,30 +1,28 @@
 import { KeyboardKeys } from "../Constants/keyboard-keys";
 
-abstract class Render {
-  private static maxDistance: number = 625;
-  private static width: number = 0.03;
-  private static height: number = 0.0065;
-  private static border: number = 0.001;
-  private static isActive: boolean = false;
+class Render {
+  private maxDistance: number = 625;
+  private width: number = 0.03;
+  private height: number = 0.0065;
+  private border: number = 0.001;
+  private isActive: boolean = false;
 
-  public static Start(): void {
+  public Start(): void {
     mp.nametags.enabled = false;
-    Render.Toogle();
-    mp.keys.bind(KeyboardKeys.Number5, true, () => Render.Toogle());
+    this.Toogle();
+    mp.keys.bind(KeyboardKeys.Number5, true, () => this.Toogle());
   }
 
-  private static Toogle(): void {
-    Render.isActive = !Render.isActive
-    if (Render.isActive) {
-      // Notify: on
-      mp.events.add(RageEnums.EventKey.RENDER, (n) => Render.Nicknames(n));
+  private Toogle(): void {
+    this.isActive = !this.isActive
+    if (this.isActive) {
+      mp.events.add(RageEnums.EventKey.RENDER, (n) => this.Nicknames(n));
     } else {
-      // Notify: off
-      mp.events.remove(RageEnums.EventKey.RENDER, (n) => Render.Nicknames(n));
+      mp.events.remove(RageEnums.EventKey.RENDER, (n) => this.Nicknames(n));
     }
   }
 
-  private static Nicknames(nametags: [PlayerMp, number, number, number][]): void {
+  private Nicknames(nametags: [PlayerMp, number, number, number][]): void {
     const graphics = mp.game.graphics;
     const screenRes = graphics.getScreenResolution(0, 0);
 
@@ -35,8 +33,8 @@ abstract class Render {
         let y: number = nametag[2];
         let distance: number = nametag[3];
 
-        if (distance <= Render.maxDistance) {
-          let scale = (distance / Render.maxDistance);
+        if (distance <= this.maxDistance) {
+          let scale = (distance / this.maxDistance);
           if (scale < 0.6) scale = 0.6;
 
           let health = player.getHealth();
@@ -54,7 +52,7 @@ abstract class Render {
               return arr2.indexOf(n) !== -1;
             });
           };
-          if (intersect(plyRoles, adminRoles).length == 0) { // if user
+          if (intersect(plyRoles, adminRoles).length == 0) {
             graphics.drawText(`${player.name.replace('_', ' ')} (${player.remoteId})`, [x, y],
               {
                 font: 4,
@@ -64,7 +62,7 @@ abstract class Render {
                 outline: true
               });
           }
-          else { // if admin
+          else {
             graphics.drawText(`[${player.getVariable('Id')}] ${player.name.replace('_', ' ')} (${player.remoteId})`, [x, y],
               {
                 font: 4,
@@ -77,22 +75,22 @@ abstract class Render {
             let y2 = y + 0.042;
 
             if (armour > 0) {
-              let x2 = x - Render.width / 2 - Render.border / 2;
+              let x2 = x - this.width / 2 - this.border / 2;
 
-              graphics.drawRect(x2, y2, Render.width + Render.border * 2, 0.0085, 0, 0, 0, 200);
-              graphics.drawRect(x2, y2, Render.width, Render.height, 150, 150, 150, 255);
-              graphics.drawRect(x2 - Render.width / 2 * (1 - health), y2, Render.width * health, Render.height, 255, 255, 255, 200);
+              graphics.drawRect(x2, y2, this.width + this.border * 2, 0.0085, 0, 0, 0, 200);
+              graphics.drawRect(x2, y2, this.width, this.height, 150, 150, 150, 255);
+              graphics.drawRect(x2 - this.width / 2 * (1 - health), y2, this.width * health, this.height, 255, 255, 255, 200);
 
-              x2 = x + Render.width / 2 + Render.border / 2;
+              x2 = x + this.width / 2 + this.border / 2;
 
-              graphics.drawRect(x2, y2, Render.width + Render.border * 2, Render.height + Render.border * 2, 0, 0, 0, 200);
-              graphics.drawRect(x2, y2, Render.width, Render.height, 41, 66, 78, 255);
-              graphics.drawRect(x2 - Render.width / 2 * (1 - armour), y2, Render.width * armour, Render.height, 48, 108, 135, 200);
+              graphics.drawRect(x2, y2, this.width + this.border * 2, this.height + this.border * 2, 0, 0, 0, 200);
+              graphics.drawRect(x2, y2, this.width, this.height, 41, 66, 78, 255);
+              graphics.drawRect(x2 - this.width / 2 * (1 - armour), y2, this.width * armour, this.height, 48, 108, 135, 200);
             }
             else {
-              graphics.drawRect(x, y2, Render.width + Render.border * 2, Render.height + Render.border * 2, 0, 0, 0, 200);
-              graphics.drawRect(x, y2, Render.width, Render.height, 150, 150, 150, 255);
-              graphics.drawRect(x - Render.width / 2 * (1 - health), y2, Render.width * health, Render.height, 255, 255, 255, 200);
+              graphics.drawRect(x, y2, this.width + this.border * 2, this.height + this.border * 2, 0, 0, 0, 200);
+              graphics.drawRect(x, y2, this.width, this.height, 150, 150, 150, 255);
+              graphics.drawRect(x - this.width / 2 * (1 - health), y2, this.width * health, this.height, 255, 255, 255, 200);
             }
           }
         }
@@ -101,4 +99,4 @@ abstract class Render {
   }
 }
 
-Render.Start();
+new Render().Start();
