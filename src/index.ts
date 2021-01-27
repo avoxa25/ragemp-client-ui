38efaf5d5eff9140106433c02c9919485507d418
Юрий Сорокин <Yura.sorokin4@yandex.ru>
2021-01-27 21:00:17 +0300

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import './CharacterSelect/main';
+
 import './Chat/main';
+
 import './HUD/main';
 import './HUD/Location/main';
+
 import './Speedometer/main';
 import './Utils/gas-station-blips-sync';
 import './Utils/house-blips-sync';
