b40bfd1d0b62d0e5631d3993e55375cf78279e9d
Юрий Сорокин <Yura.sorokin4@yandex.ru>
2021-01-27 14:05:37 +0300

    Fix

--- src/Authentication/main.ts
+++ src/Authentication/main.ts
@@ -2,32 +2,16 @@ import { RemoteEvents } from "../Constants/remote-events";
 import { LocalEvents } from "../Constants/local-events";
 import { RemoteResponse } from "../Constants/remote-response";
 
-abstract class Authentication {
-  private static browser: BrowserMp;
-  private static camera: CameraMp;
+class Authentication {
+  private readonly browser: BrowserMp;
+  private readonly camera: CameraMp;
 
-  public static Start(): void {
+  public constructor() {
     const camera = new mp.Vector3(344.3341, -998.8612, -98.19622);
     const cameraLookAt = new mp.Vector3(0.0, 0.0, 0.0);
-    Authentication.camera = mp.cameras.new('default', camera, cameraLookAt, 40);
+    this.camera = mp.cameras.new('default', camera, cameraLookAt, 40);
 
-    mp.gui.chat.show(false);
-    mp.gui.chat.activate(false);
-
-    mp.events.add(RemoteResponse.LoginAllowed, () => Authentication.Open());
-
-    mp.events.add(RemoteResponse.RegistrationSuccess, () => Authentication.Close());
-    mp.events.add(RemoteResponse.LoginSuccess, () => Authentication.Close());
-
-    mp.events.add(RemoteResponse.LoginFailed, (m: string) => Authentication.ErrorMessage(m, 'loginForm'));
-    mp.events.add(RemoteResponse.RegistrationFailed, (m: string) => Authentication.ErrorMessage(m, 'registrationForm'));
-
-    mp.events.add(LocalEvents.AuthenticationUiLogin, (u: string, p: string) => Authentication.Login(u, p));
-    mp.events.add(LocalEvents.AuthenticationUiRegistration, (u: string, e: string, p: string) => Authentication.Registration(u, e, p));
-  }
-
-  private static Open(): void {
-    Authentication.browser = mp.browsers.new('package://Authentication/authentication.html');
+    this.browser = mp.browsers.new('package://Authentication/authentication.html');
 
     mp.players.local.freezePosition(true);
 
@@ -39,28 +23,38 @@ abstract class Authentication {
 
     mp.gui.cursor.show(true, true);
 
-    Authentication.camera.setActive(true);
+    this.camera.setActive(true);
     mp.game.cam.renderScriptCams(true, false, 0, true, false);
+
+    mp.events.add(RemoteResponse.RegistrationSuccess, () => this.Close());
+    mp.events.add(RemoteResponse.LoginSuccess, () => this.Close());
+
+    mp.events.add(RemoteResponse.LoginFailed, (m: string) => this.ErrorMessage(m, 'loginForm'));
+    mp.events.add(RemoteResponse.RegistrationFailed, (m: string) => this.ErrorMessage(m, 'registrationForm'));
+
+    mp.events.add(LocalEvents.AuthenticationUiLogin, (u: string, p: string) => this.Login(u, p));
+    mp.events.add(LocalEvents.AuthenticationUiRegistration, (u: string, e: string, p: string) => this.Registration(u, e, p));
   }
 
-  private static Close(): void {
-    Authentication.browser.destroy();
+  private Close(): void {
+    this.browser.destroy();
 
-    Authentication.camera.setActive(false);
-    Authentication.camera.destroy();
+    this.camera.setActive(false);
+    this.camera.destroy();
   }
 
-  private static ErrorMessage(message: string, type: string): void {
-    Authentication.browser.execute(`window.authenticationUi.ShowErrorMessage('${type}', '${message}');`);
+  private ErrorMessage(message: string, type: string): void {
+    this.browser.execute(`window.authenticationUi.ShowErrorMessage('${type}', '${message}');`);
   }
 
-  private static Login(username: string, password: string): void {
+  private Login(username: string, password: string): void {
     mp.events.callRemote(RemoteEvents.Login, username, password);
   }
 
-  private static Registration(username: string, email: string, password: string): void {
+  private Registration(username: string, email: string, password: string): void {
     mp.events.callRemote(RemoteEvents.Registration, username, email, password)
   }
 }
 
-Authentication.Start();
+let authentication: Authentication | undefined;
+mp.events.add(RemoteResponse.LoginAllowed, () => authentication = authentication ? authentication : new Authentication());
