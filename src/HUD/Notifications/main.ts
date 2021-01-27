5683a069ca11ec611be714e4e5fdbb141d91d257
Omni92 <Tolia3646@gmail.com>
2021-01-27 14:46:23 +0200

    format fix

--- src/HUD/Notifications/main.ts
+++ src/HUD/Notifications/main.ts
@@ -2,13 +2,13 @@ import { RemoteResponse } from '../../Constants/remote-response';
 
 class Notifications {
   private static browser: BrowserMp;
-  
+
   constructor() {
     Notifications.browser = mp.browsers.new('package://Notifications/notifications.html');
     mp.events.add(RemoteResponse.NotificationSent, (notifyType: string, text: string) => this.Notify(notifyType, text));
   }
 
-  private Notify(notifyType: string, text: string ): void {
+  private Notify(notifyType: string, text: string): void {
     Notifications.browser.execute(`Notify(${notifyType},${text})`);
   }
 };
