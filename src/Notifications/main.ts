8a67d54142e5875a5b2d5d435061dcfe5e5e1958
Omni92 <Tolia3646@gmail.com>
2021-01-27 14:17:41 +0200

    Removed unneeded variable

--- src/Notifications/main.ts
+++ src/Notifications/main.ts
@@ -2,18 +2,14 @@ import { RemoteEvents } from '../Constants/remoteEvents';
 
 abstract class HudNotifications {
   private static browser: BrowserMp;
-  private static isActive: boolean;
 
   public static Start(): void {
     HudNotifications.browser = mp.browsers.new('package://Notifications/notifications.html');
-    HudNotifications.isActive = true;
     mp.events.add(RemoteEvents.NotificationSent, (notifyType: string, text: string) => HudNotifications.Notify(notifyType, text));
   }
 
   public static Notify(notifyType: string, text: string ): void {
-    if (HudNotifications.isActive) {
-      HudNotifications.browser.execute(`Notify(${notifyType},${text})`);
-    }
+    HudNotifications.browser.execute(`Notify(${notifyType},${text})`);
   }
 };
 
