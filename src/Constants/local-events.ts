ec4ee28036670c3db6d4de23e849737eef67ee70
Юрий Сорокин <Yura.sorokin4@yandex.ru>
2021-01-29 01:13:34 +0300

    Fix

--- src/Constants/local-events.ts
+++ src/Constants/local-events.ts
@@ -1,4 +1,17 @@
 export enum LocalEvents {
   ChatCursorToggle = 'Local:Chat:CursorToggle',
-  CharacterSpawnSelect = 'Local:Character:SpawnSelect'
+  CharacterSpawnSelect = 'Local:Character:SpawnSelect',
+
+  AuthenticationUiLogin = 'Local:AuthenticationUi:Login',
+  AuthenticationUiRegistration = 'Local:AuthenticationUi:Registration',
+  AuthenticationUiErrorMessage = 'Local:AuthenticationUi:ErrorMessage',
+
+  CharacterCreatorOpen = 'Local:CharacterCreator:Open',
+  CharacterCreatorTabHair = 'Local:CharacterCreator:TabHair',
+  CharacterCreatorCreate = 'Local:CharacterCreator:Create',
+  CharacterCreatorChangeGender = 'Local:CharacterCreator:ChangeGender',
+  CharacterCreatorUpdateMain = 'Local:CharacterCreator:UpdateMain',
+  CharacterCreatorUpdateClothes = 'Local:CharacterCreator:UpdateClothes',
+  CharacterCreatorUpdateFace = 'Local:CharacterCreator:UpdateFace',
+  CharacterCreatorUpdateHair = 'Local:CharacterCreator:UpdateHair'
 };
