ca5e3b3956be6d3706b9d7b216e69ec652be4683
Illia Khovanskyi <saskiuhia@gmail.com>
2021-01-31 01:10:02 +0200

    Fix notification display

--- src/HUD/Notifications/notifications.ts
+++ src/HUD/Notifications/notifications.ts
@@ -2,17 +2,37 @@ import { NotificationType } from '../../Constants/notification-type';
 
 class NotificationsUi {
   private readonly container: HTMLElement;
-  private notificationsArray = new Array;
-  private readonly maxNotifications = 3;
+  private readonly maxNotifications: number;
+  private readonly notificationShowTimeInSeconds: number;
+
+  private notifications: { type: NotificationType, text: string }[];
+  private shownNotification: number;
 
   public constructor() {
-    this.container = document.querySelector('#window') as HTMLElement;
+    this.container = document.querySelector('#notifications') as HTMLElement;
+    this.maxNotifications = 3;
+    this.notificationShowTimeInSeconds = 10;
+    this.notifications = [];
+    this.shownNotification = 0;
   }
 
   public Push(type: NotificationType, text: string): void {
-    let typeText;
+    const notification = { timeoutInterval: undefined, type: type, text: text };
+    this.notifications.push(notification);
 
-    switch (type) {
+    this.Render();
+  }
+
+  private Render(): void {
+    if (this.notifications.length === 0) return;
+    if (this.shownNotification >= this.maxNotifications) return;
+
+    const notification = this.notifications[0];
+    this.notifications = this.notifications.slice(1);
+    this.shownNotification++;
+
+    let typeText: string;
+    switch (notification.type) {
       case NotificationType.Alert:
         typeText = 'Оповещение';
         break;
@@ -26,45 +46,33 @@ class NotificationsUi {
         typeText = 'Предупреждение';
         break;
       default:
+      case NotificationType.Error:
         typeText = 'Ошибка';
         break;
     }
 
-    const notification = document.createElement('div') as HTMLElement;
-    notification.classList.add('notification');
-    notification.classList.add(type.toString().toLowerCase());
+    const notificationDivClass = notification.type.toString().toLowerCase();
+    const notificationDiv = document.createElement('div') as HTMLElement;
+    notificationDiv.classList.add('notification');
+    notificationDiv.classList.add(notificationDivClass);
+
+    const notificationTypeText = document.createElement('p') as HTMLElement;
+    notificationTypeText.innerHTML = typeText;
+    notificationDiv.appendChild(notificationTypeText);
 
     const notificationText = document.createElement('p') as HTMLElement;
-    notificationText.innerHTML = typeText;
-    notification.appendChild(notificationText);
+    notificationText.innerHTML = notification.text;
+    notificationDiv.appendChild(notificationText);
 
-    const notificationType = document.createElement('p') as HTMLElement;
-    notificationType.innerHTML = text;
-    notification.appendChild(notificationType);
+    this.container.append(notificationDiv);
 
-    const countNotifications = document.querySelectorAll('.notification');
-    if (countNotifications.length >= this.maxNotifications) {
-      this.notificationsArray.push(notification);
-      return;
-    }
-
-    this.container.appendChild(notification);
-
-    setTimeout(() => this.Delete(notification), 9999);
+    setTimeout(() => this.Expire(notificationDiv), this.notificationShowTimeInSeconds * 1000);
   }
 
-  private Delete(notification: HTMLElement): void {
-    notification.remove();
-
-    if (this.notificationsArray.length > 0) {
-      this.container.appendChild(this.notificationsArray[0]);
-      setTimeout(() => {
-        const temp = this.notificationsArray[0];
-        this.notificationsArray.shift();
-        this.Delete(temp);
-      }, 10001);
-      return;
-    }
+  private Expire(notificationDiv: HTMLElement): void {
+    notificationDiv.remove();
+    this.shownNotification--;
+    this.Render();
   }
 }
 
