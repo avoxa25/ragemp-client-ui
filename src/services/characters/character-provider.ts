1f6aca99708a29d1f3d45c13b280fb5e540433f4
Illia Khovanskyi <saskiuhia@gmail.com>
2021-01-31 21:15:34 +0200

    Drop log

--- src/services/characters/character-provider.ts
+++ src/services/characters/character-provider.ts
@@ -22,18 +22,6 @@ export abstract class CharacterProvider {
 
     const totalOnlineTimeInHours = player.getVariable('TotalOnlineTime') as number;
 
-    mp.console.logInfo(`id: ${id}`);
-    mp.console.logInfo(`accountId: ${accountId}`);
-    mp.console.logInfo(`bankAccountNumber: ${bankAccountNumber}`);
-    mp.console.logInfo(`bankCardType: ${bankCardType}`);
-    mp.console.logInfo(`firstName: ${firstName}`);
-    mp.console.logInfo(`lastName: ${lastName}`);
-    mp.console.logInfo(`hunger: ${hunger}`);
-    mp.console.logInfo(`thirst: ${thirst}`);
-    mp.console.logInfo(`cash: ${cash}`);
-    mp.console.logInfo(`rawCreatedAt: ${rawCreatedAt}`);
-    mp.console.logInfo(`totalOnlineTimeInHours: ${totalOnlineTimeInHours}`);
-
     const character = new Character(
       id, accountId,
       bankAccountNumber, bankCardType,
